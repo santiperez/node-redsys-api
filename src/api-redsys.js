b072ec6051a207a333fca9e610315d51adcfd7b8
javiertury <javiertury@gmail.com>
2019-04-25 22:41:37 +0200

    Update code and dependencies
    Some dependencies were outdated. Some coding patterns were old or
    deprecated.

--- src/api-redsys.js
+++ src/api-redsys.js
@@ -1,50 +1,43 @@
 'use strict';
 
-var crypto = require('crypto');
-var base64url = require('base64url');
+const crypto = require('crypto');
+const base64url = require('base64url');
 
-var utils = require('./utils');
-
-const zeroPad = utils.zeroPad;
-const zeroUnpad = utils.zeroUnpad;
+const { zeroPad, zeroUnpad } = require('./utils');
 
 class Redsys {
   encrypt3DES(str, key) {
-    var secretKey = new Buffer(key, 'base64');
-    var iv = new Buffer(8);
-    iv.fill(0);
-    var cipher = crypto.createCipheriv('des-ede3-cbc', secretKey, iv);
+    const secretKey = Buffer.from(key, 'base64');
+    const iv = Buffer.alloc(8, 0);
+    const cipher = crypto.createCipheriv('des-ede3-cbc', secretKey, iv);
     cipher.setAutoPadding(false);
-    var res = cipher.update(zeroPad(str, 8), 'utf8', 'base64') +
+    return cipher.update(zeroPad(str, 8), 'utf8', 'base64') +
      cipher.final('base64');
-    return res;
   }
 
   decrypt3DES(str, key) {
-    var secretKey = new Buffer(key, 'base64');
-    var iv = new Buffer(8);
-    iv.fill(0);
-    var cipher = crypto.createDecipheriv('des-ede3-cbc', secretKey, iv);
+    const secretKey = Buffer.from(key, 'base64');
+    const iv = Buffer.alloc(8, 0);
+    const cipher = crypto.createDecipheriv('des-ede3-cbc', secretKey, iv);
     cipher.setAutoPadding(false);
-    var res = cipher.update(zeroUnpad(str, 8), 'base64', 'utf8') +
+    const res = cipher.update(zeroUnpad(str, 8), 'base64', 'utf8') +
      cipher.final('utf8');
     return res.replace(/\0/g, '');
   }
 
   mac256(data, key) {
-    var hexMac256 = crypto.createHmac('sha256', new Buffer(key, 'base64'))
-    .update(data)
-    .digest('hex');
-    return new Buffer(hexMac256, 'hex').toString('base64');
+    return crypto.createHmac('sha256', Buffer.from(key, 'base64'))
+      .update(data)
+      .digest('base64');
   }
 
   createMerchantParameters(data) {
-    return new Buffer(JSON.stringify(data)).toString('base64');
+    return Buffer.from(JSON.stringify(data), 'utf8').toString('base64');
   }
 
   decodeMerchantParameters(data) {
-    var _data = JSON.parse(base64url.decode(data, 'utf8'));
-    var res = {};
+    const _data = JSON.parse(base64url.decode(data, 'utf8'));
+    const res = {};
     for (var name in _data) {
       res[decodeURIComponent(name)] = decodeURIComponent(_data[name]);
     }
@@ -52,26 +45,26 @@ class Redsys {
   }
 
   createMerchantSignature(key, data) {
-    var _data = this.createMerchantParameters(data);
-    var orderId = data.Ds_Merchant_Order || data.DS_MERCHANT_ORDER;
-    key = this.encrypt3DES(orderId, key);
-    return this.mac256(_data, key);
+    const _data = this.createMerchantParameters(data);
+    const orderId = data.Ds_Merchant_Order || data.DS_MERCHANT_ORDER;
+    const orderKey = this.encrypt3DES(orderId, key);
+    return this.mac256(_data, orderKey);
   }
 
   createMerchantSignatureNotif(key, data) {
-    var _data = this.decodeMerchantParameters(data);
-    var orderId = (_data.Ds_Order) ? _data.Ds_Order : _data.DS_ORDER;
-    key = this.encrypt3DES(orderId, key);
-    var res = this.mac256(data, key);
+    const _data = this.decodeMerchantParameters(data);
+    const orderId = _data.Ds_Order || _data.DS_ORDER;
+    const orderKey = this.encrypt3DES(orderId, key);
+    const res = this.mac256(data, orderKey);
     return base64url.encode(res, 'base64');
   }
 
   merchantSignatureIsValid(signA, signB) {
-    return base64url.decode(signA, 'base64') ==
+    return base64url.decode(signA, 'base64') ===
      base64url.decode(signB, 'base64');
   }
 }
 
 module.exports = {
-  Redsys: Redsys
+  Redsys,
 };
